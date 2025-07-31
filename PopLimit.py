ce460e6455e89fbfa17436fd4bc315976fc52c5a
LittleWildGoose <ramseytosh@gmail.com>
2025-07-30 23:09:16 -0700

    add init map

--- PopLimit.py
+++ PopLimit.py
@@ -1,6 +1,6 @@
 from AoE2ScenarioParser.scenarios.aoe2_de_scenario import AoE2DEScenario
 from utils import *
-from AoE2ScenarioParser.datasets.trigger_lists import Age, Operation
+from AoE2ScenarioParser.datasets.trigger_lists import Age, Operation, DiplomacyState
 from AoE2ScenarioParser.datasets.techs import TechInfo
 from AoE2ScenarioParser.objects.support.trigger_select import TriggerSelect, TS
 
@@ -15,12 +15,19 @@ trigger_manager = scenario.trigger_manager
 trigger_manager.remove_triggers(trigger_manager.trigger_display_order)
 
 player_manager = scenario.player_manager
+player_manager.active_players = 8
+
 for player in range(1,9):
     player_manager.players[player].food=200
     player_manager.players[player].wood=200
     player_manager.players[player].stone=200
     player_manager.players[player].gold=100
+    player_manager.players[player].allied_victory=True
+    for player2 in range(1,9):
+        if player != player2:
+            player_manager.players[player].set_player_diplomacy(player2, 0 if (player2 + player) % 2 == 0 else DiplomacyState.ENEMY )
 
+print(DiplomacyState.ENEMY)
 # WOOD = 0
 # FOOD = 1
 # GOLD = 2
@@ -35,9 +42,11 @@ age_quantity = [500, 800, 1000, 1500]
 AGE_SIZE = 4
 RESOURCE_SIZE = 4
 
-displayStr = "自己杀死或者Del的单位不会消耗人口， 可用人口低于200之后会减少人口上限"
+UPLIMIT = 500
+displayStr = "自己杀死或者Del的单位不会消耗人口， 可用人口低于200之后会减少人口上限\n \n"
 for player in range(1,9):
     displayStr = displayStr + "玩家 " + str(player) + "还有 <Variable " +str(player)+">\n"
+
 
 displayStr += "触发制作：小野鹅"
 displayTrigger = trigger_manager.add_trigger(
@@ -67,7 +76,7 @@ for player in range(1,9):
     trigger.new_condition.timer(timer=TIME_LIMIT)
 
     trigger.new_effect.change_variable(
-        quantity = 200,
+        quantity = UPLIMIT,
         operation=Operation.SET,
         variable = player
     )
@@ -96,7 +105,7 @@ for player in range(1,9):
     )
 
     trigger.new_effect.change_variable(
-        quantity = 200,
+        quantity = -200,
         operation=Operation.SET,
         variable = player + VAR2_OFFSET
     )
@@ -104,14 +113,14 @@ for player in range(1,9):
 
     trigger.new_effect.modify_variable_by_variable(
         variable2 = player,
-        operation=Operation.SUBTRACT,
+        operation=Operation.ADD,
         variable = player + VAR2_OFFSET
     )
 
     trigger.new_effect.modify_resource_by_variable(
         source_player = player,
         tribute_list = 32,
-        operation=Operation.SUBTRACT,
+        operation=Operation.SET,
         variable =  player + VAR2_OFFSET
     )
 
